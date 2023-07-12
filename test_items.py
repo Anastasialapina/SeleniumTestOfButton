aa9e905e559ded41d9d9115c9f7b95bde2766934
Anastasialapina <anastasia.lap2001@mail.ru>
2023-07-12 11:28:56 +0200

    add test of button fix

--- test_items.py
+++ test_items.py
@@ -6,7 +6,6 @@ class TestBucket:
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
-        assert button, False
         button.click()
         message = browser.find_element(By.CLASS_NAME, "alertinner").text
         assert "Coders at Work" in message, True
